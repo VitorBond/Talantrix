d9e69a41a0ca1ccc6cdbbb5acbd37f6b0995807c
Vitor-Bond <52128060+VitorBond@users.noreply.github.com>
2023-04-14 21:08:48 +0200

    Create usp_MakeFamilyPurchase.sql

---
+++ usp_MakeFamilyPurchase.sql
@@ -0,0 +1,28 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE usp_MakeFamilyPurchase  
+( @FamilySurName VARCHAR(255) )
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	
+	DECLARE @FamilyID INT
+
+	SELECT @FamilyID = [ID identity] 
+	  FROM Family 
+	 WHERE SurName LIKE @FamilySurName
+
+    IF @FamilyID IS NULL
+		PRINT('¬ведено не существующее им€ семьи!')
+	ELSE
+		UPDATE Family
+			SET BudgetValue = BudgetValue - (SELECT SUM([Value]) FROM Basket WHERE ID_Family = @FamilyID)
+		WHERE [ID identity] = @FamilyID
+		
+END
+GO
