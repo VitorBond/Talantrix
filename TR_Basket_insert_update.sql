a1a0a26a584bf0b1edb9ff1b7f9d66f3f293e67d
Vitor-Bond <52128060+VitorBond@users.noreply.github.com>
2023-04-18 15:13:53 +0200

    Update TR_Basket_insert_update.sql

--- TR_Basket_insert_update.sql
+++ TR_Basket_insert_update.sql
@@ -6,31 +6,27 @@ ON Basket
 FOR INSERT AS
 IF @@ROWCOUNT > 1
 BEGIN
+    DECLARE @ID_SKU_COUNT INT
+          , @ID_SKU INT
 
-   DECLARE @ID_SKU_COUNT INT,
-		   @ID_SKU INT
+    SELECT @ID_SKU_COUNT = COUNT(DISTINCT ID_SKU) 
+      FROM inserted;
 
-   SELECT @ID_SKU_COUNT = COUNT(DISTINCT ID_SKU) 
-     FROM inserted;
-
-   SELECT TOP (1) @ID_SKU = ID_SKU
-	 FROM inserted
-
-	IF (@ID_SKU = 1)
-		UPDATE Basket
-		SET DiscountValue = Value * 0.05
-		WHERE ID_SKU = @ID_SKU
-	
+    SELECT TOP (1) @ID_SKU = ID_SKU
+      FROM inserted
+    
+    IF (@ID_SKU = 1)
+        UPDATE Basket
+           SET DiscountValue = Value * 0.05
+         WHERE ID_SKU = @ID_SKU    
 END
 ELSE
 BEGIN
-	 SELECT TOP (1) @ID_SKU = ID_SKU
-	   FROM inserted
+    SELECT TOP (1) @ID_SKU = ID_SKU
+      FROM inserted
 
-      UPDATE Basket
-		 SET DiscountValue = 0
-	   WHERE ID_SKU = @ID_SKU
+    UPDATE Basket
+       SET DiscountValue = 0
+     WHERE ID_SKU = @ID_SKU
 END
 GO
-
-
