973b56d9f3dc1ed073b7f4ee4b17bab936c39e96
Vitor-Bond <52128060+VitorBond@users.noreply.github.com>
2023-04-18 15:15:39 +0200

    Update udf_GetSKUPrice.sql

--- udf_GetSKUPrice.sql
+++ udf_GetSKUPrice.sql
@@ -1,17 +1,17 @@
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE FUNCTION udf_GetSKUPrice 
 ( @ID_SKU INT )
 
-RETURNS DECIMAL(18, 2)
-AS
+RETURNS DECIMAL(18, 2) AS
 BEGIN
-	DECLARE @Result      DECIMAL(18, 2),
-		@SumValue    DECIMAL(18, 2),
-		@SumQuantity DECIMAL(18,2)
+	DECLARE @Result      DECIMAL(18, 2)
+	      , @SumValue    DECIMAL(18, 2)
+	      , @SumQuantity DECIMAL(18,2)
 
 	SELECT @SumValue = SUM([Value])
 	  FROM Basket
